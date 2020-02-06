3dd3c47c74025069fcb7b8581d55c34e2cc78093
Rouanet-emma <>
2020-02-06 08:48:35 +0100

    Choix par numero

--- jdvelh.c
+++ jdvelh.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+int Choix = 0;
+
+int main(){
+	printf("Bonjour voyageur. Ou souhaitez-vous aller ?\n");
+	printf("1-Miami; 2-Toronto;\n");
+	scanf("%d",&Choix);
+	
+	if(Choix==1){
+		printf("Vous etes arrives a destination de Miami.\n");
+	}
+	if(Choix==2){
+		printf("Vous etes arrives a destination de Toronto.\n");
+	}
+};
